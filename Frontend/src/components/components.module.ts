5551ae9390c554cf9810b9b2476233452d0905eb
taynara.moraes <taynara.moraes@DT-02994>
2018-11-01 14:30:54 -0300

    q

---
+++ Frontend/src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HoursMarkupComponent } from './hours-markup/hours-markup';
+@NgModule({
+	declarations: [HoursMarkupComponent],
+	imports: [],
+	exports: [HoursMarkupComponent]
+})
+export class ComponentsModule {}
